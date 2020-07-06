6d751fe5991871eca2b25ea9070ed2ea0a8cfe9b
azeemushanali <azeemushanali@gmail.com>
2020-07-07 00:05:22 +0530

    Ascii values of a character

---
+++ 3.c
@@ -0,0 +1,10 @@
+//3.	Ascii values of a character
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter the character- ");
+    scanf("%c",&c);
+    printf("The ASCII value of %c is %d .",c,c);
+    return 0;
+}
