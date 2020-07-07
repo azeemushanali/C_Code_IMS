ab46692ed47beb06e9363955217809aaa2589f83
azeemushanali <azeemushanali@gmail.com>
2020-07-08 03:31:19 +0530

    A number is even or odd

---
+++ 6.c
@@ -0,0 +1,12 @@
+//6.	A number is even or odd
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number");
+    scanf("%d",&n);
+    if (n%2==0)
+    printf("Even");
+    else
+    printf("Odd");
+}
